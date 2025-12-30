43688082a572d2e524b95e3d40d28154a0da4644
ldm20032006 <leduyminh2006k4@gmail.com>
2025-12-30 07:57:31 +0700

    Add files via upload

---
+++ HN_KS24_CNTT1_LeDuyMinh.sql
@@ -0,0 +1,78 @@
+-- Câu 1
+CREATE DATABASE IF NOT EXISTS LibraryDB;
+USE LibraryDB;
+
+DROP TABLE IF EXISTS Borrow;
+DROP TABLE IF EXISTS Book;
+DROP TABLE IF EXISTS Reader;
+
+CREATE TABLE Reader (
+    reader_id INT AUTO_INCREMENT PRIMARY KEY,
+    reader_name VARCHAR(100) NOT NULL,
+    phone VARCHAR(15) UNIQUE,
+    register_date DATE DEFAULT (CURRENT_DATE)
+);
+
+CREATE TABLE Book (
+    book_id INT PRIMARY KEY,
+    book_title VARCHAR(150) NOT NULL,
+    author VARCHAR(100),
+    publish_year INT CHECK (publish_year >= 1900)
+);
+
+CREATE TABLE Borrow (
+    reader_id INT,
+    book_id INT,
+    borrow_date DATE DEFAULT (CURRENT_DATE),
+    return_date DATE,
+    PRIMARY KEY(reader_id, book_id, borrow_date),
+    CONSTRAINT FK_Borrow_Reader FOREIGN KEY(reader_id) REFERENCES Reader(reader_id),
+    CONSTRAINT FK_Borrow_Book FOREIGN KEY(book_id) REFERENCES Book(book_id)
+);
+
+-- Câu 2
+ALTER TABLE Reader 
+ADD COLUMN email VARCHAR(100) UNIQUE;
+
+ALTER TABLE Book 
+MODIFY author VARCHAR(150);
+
+ALTER TABLE Borrow
+ADD CONSTRAINT chk_return_date
+CHECK (return_date IS NULL OR return_date >= borrow_date);
+
+-- Câu 3
+INSERT INTO Reader(reader_id, reader_name, phone, email, register_date)
+VALUES
+(1, 'Nguyễn Văn An',  '0901234567', 'an.nguyen@gmail.com',  '2024-09-01'),
+(2, 'Trần Thị Bình',  '0912345678', 'binh.tran@gmail.com',  '2024-09-05'),
+(3, 'Lê Minh Châu',   '0923456789', 'chau.le@gmail.com',   '2024-09-10');
+
+INSERT INTO Book(book_id, book_title, author, publish_year)
+VALUES
+(101, 'Lập trình C căn bản', 'Nguyễn Văn A', 2018),
+(102, 'Cơ sở dữ liệu', 'Trần Thị B', 2020),
+(103, 'Lập trình Java', 'Lê Minh C', 2019),
+(104, 'Hệ quản trị MySQL', 'Phạm Văn D', 2021);
+
+INSERT INTO Borrow(reader_id, book_id, borrow_date, return_date)
+VALUES
+(1, 101, '2024-09-15', NULL),
+(1, 102, '2024-09-15', '2024-09-25'),
+(2, 103, '2024-09-18', NULL);
+
+UPDATE Borrow
+SET return_date = '2024-10-01'
+WHERE reader_id = 1;
+
+SET SQL_SAFE_UPDATES = 0;
+UPDATE Book
+SET publish_year = 2023
+WHERE publish_year >= 2021;
+
+DELETE FROM Borrow
+WHERE borrow_date < '2024-09-18';
+
+SELECT * FROM Reader;
+SELECT * FROM Book;
+SELECT * FROM Borrow;
